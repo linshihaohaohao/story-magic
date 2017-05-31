c6420d4b85a17d93f2f8af202cda37f3fe249599
yoqu <yoqulin@qq.com>
2017-05-31 20:17:42 +0000

    init project

---
+++ src/main/java/org/yoqu/StoryMagicApplication.java
@@ -0,0 +1,12 @@
+package org.yoqu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryMagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryMagicApplication.class, args);
+	}
+}
