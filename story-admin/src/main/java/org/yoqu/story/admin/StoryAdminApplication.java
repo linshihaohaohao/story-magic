d2bf83390e6b457be448fb2ca7e5613cfa18d9fe
yoqu <wcjiang2@iflytek.com>
2017-06-04 22:47:14 +0800

    增加admin模块，更新readme

---
+++ story-admin/src/main/java/org/yoqu/story/admin/StoryAdminApplication.java
@@ -0,0 +1,12 @@
+package org.yoqu.story.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryAdminApplication.class, args);
+	}
+}
