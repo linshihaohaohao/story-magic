87d630df0ee40a1ec86d7ef526a7737a4cf8e2fe
yoqu <wcjiang2@iflytek.com>
2017-06-09 13:41:04 +0800

    指定properties

--- story-admin/src/main/java/org/yoqu/story/admin/StoryAdminApplication.java
+++ story-admin/src/main/java/org/yoqu/story/admin/StoryAdminApplication.java
@@ -1,17 +1,21 @@
 package org.yoqu.story.admin;
 
 import org.hsweb.web.core.authorize.annotation.Authorize;
+import org.hsweb.web.mybatis.MybatisProperties;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //@SpringBootApplication
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration//(exclude = MybatisAutoConfiguration.class)
 @ComponentScan(basePackages = {"org.yoqu.story"})
 @MapperScan("org.yoqu.story.admin.dao")
 @Controller
@@ -20,6 +24,13 @@ public class StoryAdminApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoryAdminApplication.class, args);
+	}
+
+	//指定使用hsweb的properties，
+	@Bean
+	@Primary
+	public MybatisProperties mybatisProperties() {
+		return new MybatisProperties();
 	}
 
 	@RequestMapping(value = {"/", "/index.html"})
