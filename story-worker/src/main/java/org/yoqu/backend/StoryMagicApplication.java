c44a258100de8d36f2af5dfc76249a45a093cf11
yoqu <yoqulin@qq.com>
2017-06-11 18:02:37 +0800

    去除多余依赖

--- story-worker/src/main/java/org/yoqu/backend/StoryMagicApplication.java
+++ story-worker/src/main/java/org/yoqu/backend/StoryMagicApplication.java
@@ -2,10 +2,11 @@ package org.yoqu.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableConfigurationProperties
+@EnableConfigurationProperties
 @EnableDiscoveryClient
 public class StoryMagicApplication {
 
